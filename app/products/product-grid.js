ea8fba5e5edd612bf42646284fdafed82f8664d3
Jamie huang <lililala0112@gmail.com>
2024-09-12 12:27:05 +0800

    product-grid product-item components done

---
+++ app/products/product-grid.js
@@ -0,0 +1,6 @@
+// import ProductItem from "./product-item";
+function productGrid({ children }) {
+  return <div className="products_items special_items">{children}</div>;
+}
+
+export default productGrid;
---
+++ app/products/product-item.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import productImg from "@/app/assets/image/bath-containers-plant-table.png";
+function productItem({ alt, imgSrc, price, name, engName, capacity, slug }) {
+  return (
+    <div className="product_item">
+      <div className="image-box image-hover">
+        <a className="product-item-image" href={`/products/${slug}`}>
+          <Image className="responsive-img" src={productImg} alt={alt} fill />
+        </a>
+        <div className="name_price info">
+          <a className="name" href="#">
+            <b>{name}</b>
+          </a>
+          <b className="price">ntd.{price}</b>
+        </div>
+        <div className="en info">
+          <small className="en_name">{engName}</small>
+          <small className="ml">{capacity}</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default productItem;
