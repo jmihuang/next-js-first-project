edc455742dabd34a00c20d3155ea6a2c7d2a6991
Jamie huang <lililala0112@gmail.com>
2024-10-12 17:28:47 +0800

    162. Use A Separate Backend

--- app/(fronted)/news/@latest/default.js
+++ app/(fronted)/news/@latest/default.js
@@ -1,11 +1,18 @@
 import NewsList from "@/app/(fronted)/news/new-list";
-import { DUMMY_NEWS } from "@/dummy-news";
-export default function LatestNewsPage() {
+
+export default async function LatestNewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    throw new Error("Fail to fetch news");
+  }
+
+  const news = await response.json();
+
   return (
     <>
       <h5>You May Also Be Interested</h5>
       <div className="latest">
-        <NewsList news={DUMMY_NEWS} />
+        <NewsList news={news} />
       </div>
     </>
   );
--- app/(fronted)/products/[slug]/@modal/default.js
+++ app/(fronted)/products/[slug]/@modal/default.js
@@ -1,3 +1,3 @@
 export default function ModalDefaultPage() {
-  return <></>;
+  return null;
 }
