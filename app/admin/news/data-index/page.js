4585f8bcd5e55218f8eb55e2b465c3ae9f5683fb
Jamie huang <lililala0112@gmail.com>
2024-11-08 18:02:59 +0800

    Error: Functions cannot be passed directly to Client Components unless you explicitly expose it by marking it with use server Or maybe you meant to call this function rather than return it.

--- app/admin/news/data-index/page.js
+++ app/admin/news/data-index/page.js
@@ -5,6 +5,37 @@ import DataTable from "@/components/data-table";
 
 export default async function DataIndex() {
   const data = await getAllNews(); // 在這裡獲取數據
-  console.log("data", data);
-  return <DataTable data={data} />;
+  const columns = [
+    {
+      title: "標題",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "圖片",
+      dataIndex: "image",
+      key: "image",
+    },
+    {
+      title: "內容",
+      dataIndex: "content",
+      key: "content",
+    },
+    {
+      title: "創建日期",
+      dataIndex: "create_time",
+      key: "創建日期",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <a href={`/edit/${record.key}`}>編輯</a>
+          <a href={`/delete/${record.key}`}>刪除</a>
+        </Space>
+      ),
+    },
+  ];
+  return <DataTable data={data} columns={columns} />;
 }
